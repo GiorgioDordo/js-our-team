d17685b71ca04e669808f3d2dbdf10af12e1906b
GiorgioDordo <djole6789@gmail.com>
2024-09-02 16:21:21 +0200

    milestone 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const candidates = [
     { firstName: 'Wayne Barnett', role: 'Founder & CEO', photo: 'wayne-barnett-founder-ceo.jpg' },
-    { firstName: 'Angela Caroll', role: 'Chief Editor', photo: 'img/angela-caroll-chief-editor.jpg' },
+    { firstName: 'Angela Caroll', role: 'Chief Editor', photo: 'angela-caroll-chief-editor.jpg' },
     { firstName: 'Walter Gordon', role: 'Office Manager', photo: 'walter-gordon-office-manager.jpg' },
     { firstName: 'Angela Lopez', role: 'Social Media Manager', photo: 'angela-lopez-social-media-manager.jpg' },
     { firstName: 'Scott Estrada', role: 'Developer', photo: 'scott-estrada-developer.jpg' },
-    { firstName: 'Barbara Ramos', role: 'Graphic Designer', photo: 'img/barbara-ramos-graphic-designer.jpg' }
+    { firstName: 'Barbara Ramos', role: 'Graphic Designer', photo: 'barbara-ramos-graphic-designer.jpg' }
 ]
 
 
@@ -13,7 +13,7 @@ for (let candidate of candidates) {
     const {firstName, role, photo} = candidate;
     console.log(firstName,"", role,"", photo);
     const printDom = document.getElementById('print-Dom');
-    printDom.innerHTML = `${firstName}, <img src="${photo}">`
+    printDom.innerHTML += `${firstName}, ${role}, <img class="photo" src="img/${photo}">`;
 }
 
 
