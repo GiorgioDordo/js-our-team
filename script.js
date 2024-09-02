84a7f820f45df09cd0a4c4b54b5a77d22acd30e2
GiorgioDordo <djole6789@gmail.com>
2024-09-02 15:05:49 +0200

    milestone 2

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const candidates = [
-    { name: 'Wayne Barnett', role: 'Founder & CEO', photo: 'wayne-barnett-founder-ceo.jpg' },
-    { name: 'Angela Caroll', role: 'Chief Editor', photo: 'angela-caroll-chief-editor.jpg' },
-    { name: 'Walter Gordon', role: 'Office Manager', photo: 'walter-gordon-office-manager.jpg' },
-    { name: 'Angela Lopez', role: 'Social Media Manager', photo: 'angela-lopez-social-media-manager.jpg' },
-    { name: 'Scott Estrada', role: 'Developer', photo: 'scott-estrada-developer.jpg' },
-    { name: 'Barbara Ramos', role: 'Graphic Designer', photo: 'barbara-ramos-graphic-designer.jpg' }
+    { firstName: 'Wayne Barnett', role: 'Founder & CEO', photo: 'wayne-barnett-founder-ceo.jpg' },
+    { firstName: 'Angela Caroll', role: 'Chief Editor', photo: 'angela-caroll-chief-editor.jpg' },
+    { firstName: 'Walter Gordon', role: 'Office Manager', photo: 'walter-gordon-office-manager.jpg' },
+    { firstName: 'Angela Lopez', role: 'Social Media Manager', photo: 'angela-lopez-social-media-manager.jpg' },
+    { firstName: 'Scott Estrada', role: 'Developer', photo: 'scott-estrada-developer.jpg' },
+    { firstName: 'Barbara Ramos', role: 'Graphic Designer', photo: 'barbara-ramos-graphic-designer.jpg' }
 ]
 
+
+
 for (let candidate of candidates) {
-    console.log(candidate.name,"", candidate.role,"", candidate.photo);
+    const {firstName, role, photo} = candidate;
+    console.log(firstName,"", role,"", photo);
+    const printDom = document.getElementById('print-Dom');
+    printDom.innerText = firstName + "  " + role + " " + photo;
 }
+
+
+
